a9a2e3f883f0cb077302ef72e73d70263b223119
modcomlearnig <modcomlearnig@gmail.com>
2020-08-12 18:16:46 +0300

    First Commit

--- BigDataEvening/Lesson1.py
+++ BigDataEvening/Lesson1.py
@@ -14,7 +14,24 @@ weight = 70.5 # float
 height = 1.4 # float
 
 name = 'Joseph' # strings,
-school = 'Modcom' # strings
+school = 'Modcom' # strin
+
+print(number)
+print(weight)
+print(school)
+
+# list - stores many values in a single variable
+shopping_list = ['Bread','Salt','Airtime','Sugar']
+print(shopping_list)
+
+tokens = [45,23,56,78,78.5,78,56,89]
+print(tokens)
+
+
+
+
+
+
 
 
 
