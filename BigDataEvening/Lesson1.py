51ee552e6b8dfbb96ddef53dfe269af77d1fa8dd
modcomlearnig <modcomlearnig@gmail.com>
2020-08-12 18:42:37 +0300

    First Commit

--- BigDataEvening/Lesson1.py
+++ BigDataEvening/Lesson1.py
@@ -26,6 +26,28 @@ print(shopping_list)
 
 tokens = [45,23,56,78,78.5,78,56,89]
 print(tokens)
+print(shopping_list[3]) # get items by index
+print(tokens[6])
+
+# tuple - stores many values in a single variable
+best_teams = ('Man U','Arsenal','Chelsea','Liverpool')
+print(best_teams)
+
+marks = (56, 78, 89.5, 45, 20, 78, 56.5)
+print(marks)
+
+print(best_teams[2]) # chelsea
+print(best_teams[1:3])  # Always a -1
+
+
+# What is difference between a list/tuple?
+# List is mutable - update
+shopping_list.append('Soap')
+shopping_list.remove('Salt')
+shopping_list.insert(3, 'Cake')
+print(shopping_list)
+
+# tuple are immutable- cannot be updated
 
 
 
