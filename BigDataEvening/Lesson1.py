dfe8ae08fd9cbbb7fca403d1006f853b884f59a2
modcomlearnig <modcomlearnig@gmail.com>
2020-08-12 18:05:56 +0300

    First Commit

--- BigDataEvening/Lesson1.py
+++ BigDataEvening/Lesson1.py
@@ -3,3 +3,22 @@ print(2000)
 print(3000)
 print('I love big data')
 
+# Variables - they store values in memory
+# data types: int, float, list, tuple, dictionary, string
+number = 45 # int
+age = 10 # int
+score = 500 # int
+points = 56 # int
+
+weight = 70.5 # float
+height = 1.4 # float
+
+name = 'Joseph' # strings,
+school = 'Modcom' # strings
+
+
+
+
+
+
+
