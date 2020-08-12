4005bb2a429568fb260626c25acd9d76379839c9
modcomlearnig <modcomlearnig@gmail.com>
2020-08-12 10:13:34 +0300

    First Commit

---
+++ BigDataMorning/Lesson1.py
@@ -0,0 +1,41 @@
+
+print(20000)
+print(5000)
+print('I Love Big data')
+
+# lets look at variables, data type
+# 5 data types: int, float, lists, tuple, dictionaries
+number = 10    # int
+age = 56       # int
+money = 200.5   # float
+weight = 75.8   # float
+
+print(number)
+print(age)
+print(money)
+print(weight)
+
+
+# list
+marks = [36,25,56,78,89,78,89,78,23,45,45.6]
+print(marks)
+
+counties =['Bomet','Nakuru','Nairobi','Kisumu']
+print(counties)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
