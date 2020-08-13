4ca0c8a288653dd8163a0c0f8986f7eba6912c19
modcomlearnig <modcomlearnig@gmail.com>
2020-08-13 17:38:13 +0300

    Second Commit

---
+++ BigDataEvening/Lesson2a.py
@@ -0,0 +1,7 @@
+
+# What is a dictionary
+# Its a data structure that use key-value approach to store data.
+person = {'jane':40, 'ken':30, 'ali':50}
+print(person)
+
+print(person['joseph'])
---
+++ BigDataEvening/Lesson2b.py
@@ -0,0 +1,15 @@
+
+# Control Statements, Functions
+# Control statements help in controlling the flow of the program.
+# if, if else, else if, while, for
+
+marks = 12
+
+if marks <= 50:
+    print('You Failed')  # works if condition is true
+
+else:
+    print('You Passed')  # Is the alternative, if condition is false
+
+
+
