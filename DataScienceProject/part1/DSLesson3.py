03b5898ec6e0270d429dbc5f3601ad73c06c3c5d
modcomlearnig <modcomlearnig@gmail.com>
2020-08-13 13:45:22 +0300

    9th Commit

--- DataScienceProject/part1/DSLesson3.py
+++ DataScienceProject/part1/DSLesson3.py
@@ -6,7 +6,6 @@
 
 import matplotlib.pyplot as plt
 import pandas
-
 # opsd_daily is the df  - dataframe
 opsd_daily  = pandas.read_csv('https://modcom.co.ke/data/datasets/power.csv',
                               parse_dates=['Date'] , index_col=0)
@@ -17,11 +16,21 @@ print(opsd_daily.isnull().sum())
 # plots
 plt.style.use('seaborn')
 
-ax= opsd_daily.loc['2017','Consumption'].plot()
-ax.set_title('Power Consumption in 2018')
+ax= opsd_daily.loc['2016',['Consumption']].plot()
+ax.set_title('Power Consumption in 2016')
 ax.set_ylabel('Power Consumption(Ghz)')
 plt.savefig('plot10.png')
 
+
+ax= opsd_daily.loc['2016',['Wind']].plot()
+ax.set_title('Wind Production in 2016')
+ax.set_ylabel('Wind (Ghz)')
+plt.savefig('plot11.png')
+
+ax= opsd_daily.loc['2016',['Solar']].plot()
+ax.set_title('Solar Production in 2016')
+ax.set_ylabel('Solar(Ghz)')
+plt.savefig('plot12.png')
 
 
 
