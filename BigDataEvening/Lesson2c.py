04cd5b76254ecfaed2106b2ee3208c78db557891
modcomlearnig <modcomlearnig@gmail.com>
2020-08-13 17:59:48 +0300

    Second Commit

--- BigDataEvening/Lesson2c.py
+++ BigDataEvening/Lesson2c.py
@@ -3,15 +3,19 @@
 # used for multiple conditions
 
 marks = float(input('What marks did you get:'))
-# anybody less 30. Failed.
-# 30 - 50  - Average Mark.
-# over 50  - 75 - ABove Average
-# Over 75   - Bravo
+
 if marks <= 30:
     print('You Failed')
 
 elif marks > 30 and marks <= 50:
     print('You got an average mark')
+
+elif marks > 50 and marks <= 75:
+    print('You got above average')
+
+elif marks > 75 and marks <= 100:
+    print('Bravo!')
+
 
 
 
