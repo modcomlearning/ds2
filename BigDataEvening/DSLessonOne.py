6b82eb8a7a31bc0a64fc13951685f08e14c666c6
modcomlearnig <modcomlearnig@gmail.com>
2020-08-17 18:12:27 +0300

    Third Commit

--- BigDataEvening/DSLessonOne.py
+++ BigDataEvening/DSLessonOne.py
@@ -3,7 +3,14 @@ import pandas
 
 # matplotlib - creating graphs, plots
 import matplotlib
-
 data = pandas.read_csv("https://modcom.co.ke/bigdataevening/datasets/iris.csv")
 print(data)
+# basic descriptive stats
+print(data.describe()) # numeric
+print(data.dtypes)
+
+# force sepalwidth to numeric
+
+data['sepalwidth'].replace({'?': 0} , inplace=True)
+pandas.to_numeric(data['sepalwidth'])
 
