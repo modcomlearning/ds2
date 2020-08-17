2fd6102257f3ea64b260c61a7b91bd7aa3dc97e4
modcomlearnig <modcomlearnig@gmail.com>
2020-08-17 17:51:40 +0300

    Third Commit

---
+++ BigDataEvening/DSLessonOne.py
@@ -0,0 +1,9 @@
+# pandas helps in data preparation.
+import pandas
+
+# matplotlib - creating graphs, plots
+import matplotlib
+
+data = pandas.read_csv("https://modcom.co.ke/bigdataevening/datasets/iris.csv")
+print(data)
+
