799fedcc9010f42e292ed83cc44f2b94c27c37a9
modcomlearnig <modcomlearnig@gmail.com>
2020-08-17 12:35:01 +0300

    10th Commit

--- DataScienceProject/part1/DSLesson4.py
+++ DataScienceProject/part1/DSLesson4.py
@@ -2,13 +2,12 @@
 
 import matplotlib.pyplot as plt
 import pandas
-# pandas.set_option('display.max_columns', 10)
+pandas.set_option('display.max_columns', 6)
 pandas.set_option('display.max_rows', 50)
 store  = pandas.read_csv('https://modcom.co.ke/data/datasets/superstore.csv')
-print(store)
-
+#print(store)
 print(store.dtypes)
 print(store.isnull().sum())
 
-# Describe data
+# Describe data - works floats and ints
 print(store.describe())
