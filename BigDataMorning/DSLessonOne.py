ef52fc2cbecb56e8eb9c99cc08948cf61796b4b0
modcomlearnig <modcomlearnig@gmail.com>
2020-08-17 10:13:21 +0300

    Fourth Commit

--- BigDataMorning/DSLessonOne.py
+++ BigDataMorning/DSLessonOne.py
@@ -10,6 +10,7 @@ import sklearn
 # pandas.pydata.org
 #print(dir(sklearn))
 # dataframe has rows and colms
+pandas.set_option('display.max_rows', 150)
 dataframe = pandas.read_csv("https://modcom.co.ke/bigdata/datasets/iris.csv")
 print(dataframe)
 
