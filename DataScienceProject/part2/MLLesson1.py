00f301f03646089a32f3bd0479766c91e8d54e0a
modcomlearnig <modcomlearnig@gmail.com>
2020-08-19 14:01:42 +0300

    11th Commit

--- DataScienceProject/part2/MLLesson1.py
+++ DataScienceProject/part2/MLLesson1.py
@@ -22,8 +22,18 @@ from sklearn import model_selection
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,
                                                             test_size=validation_size,
                                                                     random_state=seed)
-
 # X, Y will be splitted into X_train, X_test, Y_train, Y_test
+# X_train holds the 70% of the features
+# X_test holds the 30% of the features
+
+# Y_train holds the 70% of the output
+# Y_test holds the 30% of the output
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+model = KNeighborsClassifier()
+model.fit(X_train, Y_train) # done, learning from 70%
 
 
 
