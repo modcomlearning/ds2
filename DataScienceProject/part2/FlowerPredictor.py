aa9670749cafcb1b7b7998cad8d926a18485b285
modcomlearnig <modcomlearnig@gmail.com>
2020-08-20 12:50:39 +0300

    First Commit

--- DataScienceProject/part2/FlowerPredictor.py
+++ DataScienceProject/part2/FlowerPredictor.py
@@ -14,6 +14,22 @@ Y = array[:, 4]  # 4th counted here: outcome
 
 # Step 2: split data into training set and test set.
 # 70% for training , 30% for test set
+from sklearn import model_selection
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,
+                                                                test_size=0.30,
+                                                                random_state=42)
+
+
+# Step 3: Create algorithms/model and expose the X_train, Y_train = 70% of data
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+
 
 
 
