9c0fa52106d6db124912a3ba0bba9fa9aa144025
modcomlearnig <modcomlearnig@gmail.com>
2020-08-20 17:54:02 +0300

    Third Commit

--- BigDataEvening/FlowerPredictor.py
+++ BigDataEvening/FlowerPredictor.py
@@ -11,4 +11,12 @@ X = array[:, 0:4]   # upto the 3rd colm, always a -1  -Features
 Y = array[:, 4]     # outcome/output
 
 # Step 2: Split your X, Y into training set and testing set
+from sklearn import model_selection
+# 70% of 150 rows - training  set
+# 30% of 150 rows - testing set
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,
+                                                                    test_size=0.30,
+                                                                    random_state=7)
+
+
 
