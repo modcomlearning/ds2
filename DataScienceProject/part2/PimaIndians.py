c15f6762a9f34c5bb6d61467d60dcfa2866641aa
modcomlearnig <modcomlearnig@gmail.com>
2020-08-24 12:56:49 +0300

    First Commit

--- DataScienceProject/part2/PimaIndians.py
+++ DataScienceProject/part2/PimaIndians.py
@@ -15,12 +15,28 @@ array = dataframe.values
 X = array[:, 0:8] # always a -1 , upto 7: features
 Y = array[:, 8]  # 8th counted here: outcome
 
+
+# class imbalance - positives are less than negatives
+# create a virtual records/artificial data
+import imblearn
+# Synthetic Minority Oversamplng Technique
+from imblearn.over_sampling import SMOTE
+smote = SMOTE(random_state=42)
+
+X_new, Y_new = smote.fit_sample(X,Y)
+
+
+
+
 # Step 2: split data into training set and test set.
 # 70% for training , 30% for test set
 from sklearn import model_selection
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,
                                                                 test_size=0.30,
                                                                 random_state=42)
+
+
+
 
 
 # Step 3: Create algorithms/model and expose the X_train, Y_train = 70% of data
@@ -32,7 +48,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 
-model = GaussianNB()
+model = RandomForestClassifier(n_estimators=40)
 model.fit(X_train, Y_train) # we hide x_test, y_test from the model
 print('Training....') # above is the learning part
 
@@ -49,3 +65,14 @@ print(classification_report(Y_test,predictions))
 
 from sklearn.metrics import confusion_matrix
 print(confusion_matrix(Y_test,predictions))
+
+# machine algorithms do not work with strings, change them to 0,1.
+# All string data must be encoded to 0,1s
+
+
+
+
+
+
+
+
