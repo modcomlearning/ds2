3e2a1d406dcf41734599a40cf1773f90dccda178
modcomlearnig <modcomlearnig@gmail.com>
2020-08-24 10:33:08 +0300

    First Commit

--- BigDataMorning/Regression.py
+++ BigDataMorning/Regression.py
@@ -6,9 +6,8 @@ print(dataframe)
 
 # Step 1: split your data to X, Y
 array = dataframe.values   # get all values in your dataframe
-X = array[:, 1:4]   # means 0 - 7 : Features
+X = array[:, 1:4]   # means 1-3 : Features
 Y = array[:, 4]    # label/outcome/continous/
-
 # Step 2: split your data to train/test
 from sklearn import model_selection
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,
@@ -19,3 +18,16 @@ X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y,
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
+model = LinearRegression()
+model.fit(X_train, Y_train)
+print('Training....')
+
+# STep 4: testing
+
+predictions = model.predict(X_test)
+print(predictions)
+
+# compare predictions with y_test
