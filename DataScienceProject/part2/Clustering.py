46ec5bbedc3082e377dd8aa6cfd7e2faeca425b5
modcomlearnig <modcomlearnig@gmail.com>
2020-08-26 13:24:15 +0300

    First Commit

--- DataScienceProject/part2/Clustering.py
+++ DataScienceProject/part2/Clustering.py
@@ -3,3 +3,10 @@
 import pandas
 dataframe = pandas.read_csv("https://modcom.co.ke/data/datasets/AirlinesCluster.csv")
 print(dataframe)
+
+array = dataframe.values
+X = array[:, 0:7]  # always a minus 1, its 6
+# No Y
+
+from sklearn.cluster import KMeans
+model = KMeans()
