cbd7432878ea788b9fa53e9630400058ca4515f7
modcomlearnig <modcomlearnig@gmail.com>
2020-08-26 13:38:24 +0300

    First Commit

--- DataScienceProject/part2/Clustering.py
+++ DataScienceProject/part2/Clustering.py
@@ -5,10 +5,16 @@ dataframe = pandas.read_csv("https://modcom.co.ke/data/datasets/AirlinesCluster.
 print(dataframe)
 
 array = dataframe.values
-X = array[:, 0:7]  # always a minus 1, its 6
+X = array[:, 4:7]  # always a minus 1, its 6
 # No Y
 from sklearn.cluster import KMeans
 model = KMeans(random_state=42, n_clusters=10)
 model.fit(X)
 print('Model Clustering ....Done!')
 
+# view the clusters
+centronoids = model.cluster_centers_
+clusters= pandas.DataFrame(centronoids,
+                           columns=['FlightMiles','FlightTrans','DaysSinceEnroll'])
+
+print(clusters)
