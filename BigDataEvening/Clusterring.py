228866603f941f155df00ec54bcbbbc92e5ad614
modcomlearnig <modcomlearnig@gmail.com>
2020-08-26 17:47:52 +0300

    Third Commit

--- BigDataEvening/Clusterring.py
+++ BigDataEvening/Clusterring.py
@@ -4,3 +4,7 @@
 import pandas
 dataframe =pandas.read_csv("https://modcom.co.ke/bigdataevening/datasets/AirlinesCluster.csv")
 print(dataframe)
+
+# We find  customers with similar x-tics, narrow to clusters
+subset = dataframe[['FlightMiles','FlightTrans','DaysSinceEnroll']]
+print(subset)
