416f4d44195504de911c87f8a23f59e2e71ebf7a
modcomlearnig <modcomlearnig@gmail.com>
2020-08-26 09:49:37 +0300

    First Commit

--- BigDataMorning/Clustering.py
+++ BigDataMorning/Clustering.py
@@ -5,3 +5,8 @@ import pandas
 
 dataframe = pandas.read_csv("https://modcom.co.ke/bigdata/datasets/AirlinesCluster.csv")
 print(dataframe)
+
+# Step 1:
+subset = dataframe[['FlightMiles','FlightTrans','DaysSinceEnroll']]
+print(subset)
+
