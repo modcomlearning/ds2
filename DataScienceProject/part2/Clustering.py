e18a4bd0ce53d0f8a4a5042916b7234144f9d5a7
modcomlearnig <modcomlearnig@gmail.com>
2020-08-26 13:30:31 +0300

    First Commit

--- DataScienceProject/part2/Clustering.py
+++ DataScienceProject/part2/Clustering.py
@@ -7,6 +7,8 @@ print(dataframe)
 array = dataframe.values
 X = array[:, 0:7]  # always a minus 1, its 6
 # No Y
-
 from sklearn.cluster import KMeans
-model = KMeans()
+model = KMeans(random_state=42, n_clusters=10)
+model.fit(X)
+print('Model Clustering ....Done!')
+
