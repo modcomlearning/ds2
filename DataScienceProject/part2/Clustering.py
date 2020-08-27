15591ff6fa2800c9f09d7a8f794426c4ae942119
modcomlearnig <modcomlearnig@gmail.com>
2020-08-27 13:14:48 +0300

    First Commit

--- DataScienceProject/part2/Clustering.py
+++ DataScienceProject/part2/Clustering.py
@@ -7,6 +7,24 @@ print(dataframe)
 array = dataframe.values
 X = array[:, 4:7]  # always a minus 1, its 6
 # No Y
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10, 8))
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 20):
+     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+     kmeans.fit(X)
+     wcss.append(kmeans.inertia_)
+plt.plot(range(1, 20), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.savefig("fig2.png")
+
+
+
+
 from sklearn.cluster import KMeans
 model = KMeans(random_state=42, n_clusters=10)
 model.fit(X)
