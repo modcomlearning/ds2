1cd4c98b91a547496e276299689be5348a953b76
modcomlearnig <modcomlearnig@gmail.com>
2020-09-10 12:21:58 +0300

    First Commit

--- BigDataMidday/Lesson2.py
+++ BigDataMidday/Lesson2.py
@@ -5,19 +5,19 @@
 # Operators: Logical : and, or , not
 # Operators: Membership: in , not in
 # Operators: Identity:  is , is not
-
+import math
 
 time = 24
 principle = 60000
 rate = 1.5
-time = 24
 name = 'Kelvin'
 
-interest = principle * rate *time/100
+interest = principle * rate *time/112
 print('Your Name: ', name)
 print('Your Interest ', interest, 'KES')
 
-
+formatted_float = "{:.3f}".format(interest)
+print(formatted_float)
 
 
 
