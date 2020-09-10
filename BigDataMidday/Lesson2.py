1966b9ca540af4896298705339301e777c976693
modcomlearnig <modcomlearnig@gmail.com>
2020-09-10 12:14:16 +0300

    First Commit

--- BigDataMidday/Lesson2.py
+++ BigDataMidday/Lesson2.py
@@ -1,19 +1,22 @@
 
 # Operators, Control statements
-# Operators: Arithmetic +, -, /, *
+# Operators: Arithmetic +, -, /, *, %,
 # Operators: Comparison: > , < , >=, <=, ==, !=
 # Operators: Logical : and, or , not
 # Operators: Membership: in , not in
 # Operators: Identity:  is , is not
 
+
+time = 24
 principle = 60000
 rate = 1.5
-time = 24 
+time = 24
 name = 'Kelvin'
 
 interest = principle * rate *time/100
-print(name)
-print(interest)
+print('Your Name: ', name)
+print('Your Interest ', interest, 'KES')
+
 
 
 
