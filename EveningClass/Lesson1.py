1e081741357c0bfd18af43e65df1b95dd8a4f208
Beth Miano <bethernest50@gmail.com>
2020-09-18 18:40:02 +0300

    First Commit

--- EveningClass/Lesson1.py
+++ EveningClass/Lesson1.py
@@ -14,9 +14,19 @@ print(town)
 # think of variables as containers that hold values
 
 # tuple - store many values in a single weight variable
-weight = (20.5,30,25.9,23,21,15,14,10,12,10.5,78,45,66,44,22,44,55,44,55,98)
+weight = (20.5,30,25.9,23,21,15,14,10,12,10.5,78,45,66,44,22,44,55,44,55,98,'Sky')
 print(weight)
 print(weight[9])
+
+teams = ('Man U','Chelsea','Arsenal','Man city','Liverpool')
+scores = (2,6,5,3)
+
+
+
+
+
+
+
 
 
 
