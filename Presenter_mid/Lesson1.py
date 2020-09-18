63daf580fe3b420feff04215adad82ddbb629da3
Beth Miano <bethernest50@gmail.com>
2020-09-18 12:14:49 +0300

    First Commit

---
+++ Presenter_mid/Lesson1.py
@@ -0,0 +1,20 @@
+print(1000)
+
+# variables - store values
+# Data types: int(whole numbers), float(decimals), string(xters),lists, tuples, dictionaries
+temp = 36.5  # float
+height = 1.5 # float
+
+age = 20  # int
+marks = 56 # int
+
+print(temp)
+print(height)
+print(age)
+print(marks)
+
+
+
+
+
+
