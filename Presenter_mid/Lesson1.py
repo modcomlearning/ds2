9c2ff24bf05a9f1062c111ccc4950bcdf0d72045
Beth Miano <bethernest50@gmail.com>
2020-09-18 12:39:01 +0300

    Second Commit

--- Presenter_mid/Lesson1.py
+++ Presenter_mid/Lesson1.py
@@ -17,9 +17,16 @@ name = 'Joseph Modcom' # string
 print(name)
 
 # tuple store may values in one variable
-points = (78,78,55,66,22,77,88,55,11,22,55,66,78,99,78.9,'Ann')
+points = (78,78,55,66,22,77,88,55,11,22,55,66,78,99,78.9,'Ann')  # tuple/array
 print(points)
+print(points[2])
 
+total = points[0] + points[4]
+print(total)
+
+counties = ('Embu','Kisumu','Nairobi','Nandi','Kajiado','Nakuru','Meru')
+print(counties)
+print(counties[0])
 
 
 
