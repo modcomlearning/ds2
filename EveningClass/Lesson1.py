1150026aad47652bec86a0d6fbc68841729f3b31
Beth Miano <bethernest50@gmail.com>
2020-09-18 18:24:31 +0300

    First Commit

---
+++ EveningClass/Lesson1.py
@@ -0,0 +1,17 @@
+print(10000)
+# syntax rules used in programming
+# reserved keywords   - words used by programming to perform specific task
+# variables storing information/data/values*
+# data types: int, float(decimals), string, tuples, lists, dictionaries
+
+age =  34  # int
+marks = 78.5  # float
+town = 'Joseph'
+
+print(age)
+print(marks)
+print(town)
+# think of variables as containers that hold values
+
+
+
