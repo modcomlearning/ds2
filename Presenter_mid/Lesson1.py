807b1ce039f05c12a32ab2335337c91187456201
Beth Miano <bethernest50@gmail.com>
2020-09-19 12:02:23 +0300

    Second Commit

--- Presenter_mid/Lesson1.py
+++ Presenter_mid/Lesson1.py
@@ -48,3 +48,4 @@ print(teams)
 
 
 
+
---
+++ Presenter_mid/Lesson2b.py
@@ -0,0 +1,21 @@
+
+# functions and control statements
+# function = its a block of code performing some related tasks
+# Advantages of functions - makes you code simple/easy to understand
+# Make your code easy to maintain
+# Functions makes it easier to work with OOP
+# We do a function to find SI
+def simple_interest():
+    principle = 60000
+    rate = 7.9
+    time = 24
+    yourname = 'Joe'
+
+    # arithmetic operators   +, -,/,*, %(Modulus)  2%2 = 0
+    answer = (principle * rate * time)/100
+    print('Your SImple Interest is ', answer)
+
+    
+
+
+
