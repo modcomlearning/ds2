cc2865acb04dd00b2db1217b44ad895630eccdd3
Beth Miano <bethernest50@gmail.com>
2020-09-19 12:54:21 +0300

    Third Commit

--- Presenter_mid/Lesson2b.py
+++ Presenter_mid/Lesson2b.py
@@ -19,10 +19,28 @@ def simple_interest():
 simple_interest()  # a function call/run/invoke
 
 def body_mass_index():
-    height = 1.6
-    weight = 75.5
-    bmi = weight/(height*height) * 703
+    height = 1.6 # m
+    weight = 60.5 # kgs
+    bmi = weight/(height*height)
     print('Your BMI is ', bmi)
+    # Control statements
+    # Comparison operators: > , >=, <, <=, ==, != (Not equal)
+    # Logical Operators: and, or, not
+    # use if, if else, elif
+    if bmi < 18.5:
+        print('Underweight')
+
+    elif bmi >= 18.5 and bmi <= 24.9:
+        print('Normal')
+
+    elif bmi >= 25 and bmi <=29.9:
+        print('Overweight')
+
+    elif bmi >=30 and bmi <=100:
+        print('Very Overweight')
+
+    else:
+        print('Invalid BMI')  # out of our conditions
 
 
 body_mass_index()
