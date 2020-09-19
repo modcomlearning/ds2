3d115b9019e458f98bca995a42cf3a2340728de9
Beth Miano <bethernest50@gmail.com>
2020-09-19 17:42:55 +0300

    First Commit

--- EveningClass/Lesson2.py
+++ EveningClass/Lesson2.py
@@ -5,10 +5,15 @@ list = ['Beta','Alpha','Omega']
 print(tuple)
 print(list)
 print(tuple[2])
-
 # Lists(mutable - updated) vs tuple(immutable - cant updated)
 list.append("Theta")
 list.append("Delta")
 list.remove("Beta")
-
 print(list)
+
+# dictionary   - stores using key - value  : key carries the value
+person ={'john':40, 'peter':45, 'ann':38}
+print(person['susan'])
+# exception - affects a flow of  your program, something unusual
+
+
