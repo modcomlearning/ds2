f250da914b0243ca491ef2099f1fd7fee8ef22da
Beth Miano <bethernest50@gmail.com>
2020-09-19 18:04:46 +0300

    First Commit

---
+++ EveningClass/Lesson2a.py
@@ -0,0 +1,12 @@
+# functions and control statements
+# A functions is a block of statements performing specific task
+# Advantages: makes easy to understand and maintain, used OOP, improve code reuse.
+def simple_interest():
+    principle = 7000
+    rate = 2.5
+    time = 24
+    answer = principle * rate * time
+    print('Your Simple interest is: ', answer)
+
+
+
